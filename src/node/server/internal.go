18aa83b4d0c5d2599cdd839ed47e3e17346196df
utterwqlnut <dhruva.chayapathy@gmail.com>
2026-04-13 16:41:51 -0400

    mutex sync for internal store, however, this prob wont work

--- src/node/server/internal.go
+++ src/node/server/internal.go
@@ -1,6 +1,9 @@
 package server
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 type internalKeyValueStore struct {
 	store  map[string]any
@@ -12,4 +15,42 @@ func newInternalKeyValueStore() *internalKeyValueStore {
 	return &internalKeyValueStore{
 		store: make(map[string]any),
 	}
+}
+
+func (kv *internalKeyValueStore) get(key string) (any, error) {
+	kv.mut.RLock()
+
+	value, ok := kv.store[key]
+
+	kv.mut.RUnlock()
+
+	if ok != true {
+		return nil, errors.New("Key not in the store")
+	}
+
+	return value, nil
+}
+
+func (kv *internalKeyValueStore) write(key string, value any) {
+	kv.mut.Lock()
+
+	kv.store[key] = value
+
+	kv.mut.Unlock()
+}
+
+func (kv *internalKeyValueStore) erase(key string) error {
+	kv.mut.Lock()
+
+	_, ok := kv.store[key]
+
+	if ok != true {
+		kv.mut.Unlock()
+		return errors.New("Key not in the store")
+	}
+
+	delete(kv.store, key)
+	kv.mut.Unlock()
+
+	return nil
 }
